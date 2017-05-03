558fcba765cd85aea0388a8fbdb4716d51100795
GostevD <exalted.aims@gmail.com>
2017-05-03 13:32:58 +0300

    remove svg symbol

--- gulpfile.js
+++ gulpfile.js
@@ -1,187 +1,165 @@
 "use strict";
 
-var gulp = require('gulp'),
+var gulp        = require('gulp'),
     browserSync = require('browser-sync'),
-    reload = browserSync.reload,
-    del = require('del'),
-    pug = require('gulp-pug'),
-    cache = require('gulp-cache'),
-    notify = require('gulp-notify'),
-    imgMin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    gcmq = require('gulp-group-css-media-queries'),
-    csso = require('gulp-csso'),
-    useref = require('gulp-useref'),
-    gulpif = require('gulp-if'),
-    uglifyJs = require('gulp-uglifyjs'),
-    svgSprite = require('gulp-svg-sprite'),
-		path           = require('path'),
-    compass = require('gulp-compass');
+    reload      = browserSync.reload,
+    del         = require('del'),
+    pug         = require('gulp-pug'),
+    cache       = require('gulp-cache'),
+    notify      = require('gulp-notify'),
+    imgMin      = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    gcmq        = require('gulp-group-css-media-queries'),
+    csso        = require('gulp-csso'),
+    useref      = require('gulp-useref'),
+    gulpif      = require('gulp-if'),
+    uglifyJs    = require('gulp-uglifyjs'),
+    svgSprite   = require('gulp-svg-sprite'),
+    path        = require('path'),
+    compass     = require('gulp-compass');
 
 //---------------------------------------SYNC
 
-gulp.task('sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "app/"
-        },
-        notify: false
-    });
+gulp.task('sync', function () {
+	browserSync.init({
+		server: {
+			baseDir: "app/"
+		},
+		notify: false
+	});
 });
 
 //---------------------------------------PUG
 
-gulp.task('pug', function() {
-    return gulp.src('app/*.pug')
-        .pipe(pug({
-            pretty: true
-        }))
-        .on('error', notify.onError({
-            message: "<%= error.message %>",
-            title: "Pug error!"
-        }))
-        .pipe(gulp.dest('app/'));
+gulp.task('pug', function () {
+	return gulp.src('app/*.pug')
+		.pipe(pug({
+			pretty: true
+		}))
+		.on('error', notify.onError({
+			message: "<%= error.message %>",
+			title  : "Pug error!"
+		}))
+		.pipe(gulp.dest('app/'));
 });
 
 //---------------------------------------CSS
 
-gulp.task('compass', function() {
-    gulp.src('app/sass/**/*.sass')
-        .pipe(compass({
-			project: path.join(__dirname, 'app'),
-            http_path: '/local',
-            comments: false,
-            css: 'css',
-            image: 'images',
-            javascript: 'js',
-            sass: 'sass',
-            relative: true  // change to false before build
-        }))
-        .on('error', notify.onError({
-            message: "<%= error.message %>",
-            title: "Compass error!"
-        }))
-        .pipe(gulp.dest('css'))
-        .pipe(reload({ stream: true }));
+gulp.task('compass', function () {
+	gulp.src('app/sass/**/*.sass')
+		.pipe(compass({
+			project   : path.join(__dirname, 'app'),
+			http_path : '/local',
+			comments  : false,
+			css       : 'css',
+			image     : 'images',
+			javascript: 'js',
+			sass      : 'sass',
+			relative  : true  // change to false before build
+		}))
+		.on('error', notify.onError({
+			message: "<%= error.message %>",
+			title  : "Compass error!"
+		}))
+		.pipe(gulp.dest('css'))
+		.pipe(reload({stream: true}));
 });
 
 //---------------------------------------IMAGES
 
-gulp.task('img', function() {
-    return gulp.src(['app/images/**/*', '!app/images/svg/*', '!app/images/sprite.svg'])
-        .pipe(cache(imgMin({
-            intarlaced: true,
-            progressive: true,
-            svgoPlugins: [{ removeViewBox: false }],
-            une: [pngquant()]
-        })))
-        .pipe(gulp.dest('dist/images'));
-});
-
-gulp.task('svgSymol', function() {
-    return gulp.src('app/images/svg/*.svg')
-        .pipe(svgSprite({
-            mode: {
-                symbol: {
-                    dest: '',
-                    sprite: 'sprite',
-                    render: {
-                        scss: {
-                            dest: '../sass/_sprite.scss'
-                        }
-                    }
-                }
-            }
-        }))
-        .on('error', notify.onError({
-            message: "<%= error.message %>",
-            title: "SVG error!"
-        }))
-        .pipe(gulp.dest('app/images/'));
+gulp.task('img', function () {
+	return gulp.src(['app/images/**/*', '!app/images/svg/*', '!app/images/sprite.svg'])
+		.pipe(cache(imgMin({
+			intarlaced : true,
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+			une        : [pngquant()]
+		})))
+		.pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('svgCss', function () {
-    return gulp.src('app/images/svg/css/*.svg')
-        .pipe(svgSprite({
-            mode: {
-                css: {
-                    dest  : '',
-                    sprite: '../images/sprite-css',
-                    render: {
-                        scss: {
-                            dest: '../sass/_sprite-css.scss'
-                        }
-                    },
-                    bust  : false
-                }
-            }
-        }))
-        .on('error', notify.onError(
-            {
-                message: "<%= error.message %>",
-                title  : "SVG error!"
-            }
-        ))
-        .pipe(gulp.dest('app/images/'));
+	return gulp.src('app/images/svg/**/*.svg')
+		.pipe(svgSprite({
+			mode: {
+				css: {
+					dest  : '',
+					sprite: '../images/sprite-css',
+					render: {
+						scss: {
+							dest: '../sass/_sprite-css.scss'
+						}
+					},
+					bust  : false
+				}
+			}
+		}))
+		.on('error', notify.onError(
+			{
+				message: "<%= error.message %>",
+				title  : "SVG error!"
+			}
+		))
+		.pipe(gulp.dest('app/images/'));
 });
 
 //---------------------------------------CLEAN
 
-gulp.task('clean', function() {
-    return del.sync('dist');
+gulp.task('clean', function () {
+	return del.sync('dist');
 });
 
-gulp.task('cleanAppCss', function() {
-    return del.sync('app/css');
+gulp.task('cleanAppCss', function () {
+	return del.sync('app/css');
 });
 
-gulp.task('cleanCss', function() {
-    return del.sync('dist/css');
+gulp.task('cleanCss', function () {
+	return del.sync('dist/css');
 });
 
 //---------------------------------------WATCH
 
-gulp.task('watch', ['sync', 'compass', 'pug', 'svgSymbol', 'svgCss'], function() {
-    gulp.watch('app/sass/**/*.sass', ['compass']);
-    gulp.watch('app/**/*.pug', ['pug']);
-    gulp.watch('app/index.html', reload);
-    gulp.watch('app/**/*.js', reload);
+gulp.task('watch', ['sync', 'compass', 'pug', 'svgCss'], function () {
+	gulp.watch('app/sass/**/*.sass', ['compass']);
+	gulp.watch('app/**/*.pug', ['pug']);
+	gulp.watch('app/index.html', reload);
+	gulp.watch('app/**/*.js', reload);
 });
 
 //---------------------------------------BUILD
 
-gulp.task('build', ['clean', 'img', 'pug', 'svgSymbol', 'svgCss', 'compass'], function() {
+gulp.task('build', ['clean', 'img', 'pug', 'svgCss', 'compass'], function () {
 
-    gulp.src('app/css/**/*.css')
-        .pipe(gcmq())
-        .pipe(csso({
-            restructure: false,
-            sourceMap: false,
-            debug: true
-        }))
-        .pipe(gulp.dest('dist/css'));
-			
+	gulp.src('app/css/**/*.css')
+		.pipe(gcmq())
+		.pipe(csso({
+			restructure: false,
+			sourceMap  : false,
+			debug      : true
+		}))
+		.pipe(gulp.dest('dist/css'));
 
-    gulp.src('app/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'));
 
-    gulp.src('app/*.html')
-        .pipe(useref())
-        .pipe(gulpif('*.js', uglifyJs()))
-        .pipe(gulp.dest('dist'));
+	gulp.src('app/fonts/**/*')
+		.pipe(gulp.dest('dist/fonts'));
 
-    gulp.src('app/images/sprite.svg')
-        .pipe(gulp.dest('dist/images'));
+	gulp.src('app/*.html')
+		.pipe(useref())
+		.pipe(gulpif('*.js', uglifyJs()))
+		.pipe(gulp.dest('dist'));
+
+	gulp.src('app/images/sprite.svg')
+		.pipe(gulp.dest('dist/images'));
 
 });
 
-gulp.task('buildCss', ['cleanCss', 'compass'], function() {
-    gulp.src('app/css/**/*.css')
-        .pipe(gcmq())
-        .pipe(csso({
-            restructure: false,
-            sourceMap: false,
-            debug: true
-        }))
-        .pipe(gulp.dest('dist/css'));
+gulp.task('buildCss', ['cleanCss', 'compass'], function () {
+	gulp.src('app/css/**/*.css')
+		.pipe(gcmq())
+		.pipe(csso({
+			restructure: false,
+			sourceMap  : false,
+			debug      : true
+		}))
+		.pipe(gulp.dest('dist/css'));
 });
